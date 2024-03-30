a68b229b13a9a192f3c9be2f035451123bf29bb6
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-30 14:24:06 +0530

    create database

---
+++ create and drop database.sql
@@ -0,0 +1,15 @@
+
+-- CREATE and DROP DATABASE
+
+SHOW DATABASES;
+USE information_schema;
+SHOW TABLES;
+CREATE DATABASE temp1;
+USE temp1;
+DROP DATABASE temp1;
+
+-- CREATE DATABASE - college 
+SHOW DATABASES;
+CREATE DATABASE college;
+USE college;
+SHOW TABLES;  -- shows all the tables inside the selected database
